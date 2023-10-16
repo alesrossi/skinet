94f9a105cb46833bb37d2d3163ebc5e005fc66fb
dev <alessandro0024@gmail.com>
2023-10-16 20:24:35 +0200

    Changed functionality of product details

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -4,6 +4,7 @@ import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { BasketService } from '../../basket/basket.service';
+import { take } from 'rxjs/internal/operators/take';
 
 @Component({
   selector: 'app-product-details',
@@ -13,6 +14,7 @@ import { BasketService } from '../../basket/basket.service';
 export class ProductDetailsComponent implements OnInit {
   product: Product;
   quantity = 1;
+  quantityInBasket = 0;
 
   constructor(
     private shopService: ShopService,
@@ -27,8 +29,22 @@ export class ProductDetailsComponent implements OnInit {
     this.loadProduct();
   }
 
-  addItemToBasket() {
-    this.basketService.addItemToBasket(this.product, this.quantity);
+  updateBasket() {
+    if (this.product) {
+      if (this.quantity > this.quantityInBasket) {
+        const itemsToAdd = this.quantity - this.quantityInBasket;
+        this.quantityInBasket += itemsToAdd;
+        this.basketService.addItemToBasket(this.product, itemsToAdd);
+      } else {
+        const itemsToRemove = this.quantityInBasket - this.quantity;
+        this.quantityInBasket -= itemsToRemove;
+        this.basketService.removeItemFromBasket(this.product.id, itemsToRemove);
+      }
+    }
+  }
+
+  get buttonText() {
+    return this.quantityInBasket === 0 ? 'Add to basket' : 'Update basket';
   }
 
   incrementQuantity() {
@@ -36,22 +52,30 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   decrementQuantity() {
-    if (this.quantity > 1) {
+    if (this.quantity > 0) {
       this.quantity--;
     }
   }
 
   loadProduct() {
-    this.shopService
-      .getProduct(+this.activatedRoute.snapshot.paramMap.get('id'))
-      .subscribe(
-        (product) => {
-          this.product = product;
-          this.bcService.set('@productDetails', product.name);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.shopService.getProduct(id).subscribe({
+      next: (product) => {
+        this.product = product;
+        this.bcService.set('@productDetails', product.name);
+        this.basketService.basketSource$.pipe(take(1)).subscribe({
+          next: (basket) => {
+            const item = basket.items.find((x) => x.id === id);
+            if (item) {
+              this.quantity = item.quantity;
+              this.quantityInBasket = item.quantity;
+            }
+          },
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 }
